b8a4c215c4fe5ab343b3c47a79aa10376a3eb7fc
LastCleanShirt <darealfoxcat123@gmail.com>
2025-08-27 11:20:59 +0700

    added the card text thingy

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ document.addEventListener("DOMContentLoaded", function(){
     
     function typeWriter(){
         if (i<welcometext.length)
-        {
+            {
             document.getElementById("lets-connect").innerHTML += welcometext.charAt(i);
             i++;
             setTimeout(typeWriter, speed);
         }
     }
-
+    
     typeWriter();
+    
+    const tl_cards = document.querySelectorAll(".timeline-item");
+    
+    tl_cards.forEach(card => {
+        card.addEventListener("click", function() {
+            // Find the <p> inside this timeline-item
+            const hiddenText = this.querySelector(".card-desc");
+            if (hiddenText) {
+                // Toggle between display: none and block
+                hiddenText.style.display = hiddenText.style.display === "block" ? "none" : "block";
+            }
+        });
+    });
 })
